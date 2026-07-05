d3f7470bac38499a71c093657f8e131901107f98
Gomeye <maiyegift@gmail.com>
2026-07-05 03:54:34 +0100

    fix: add tsconfig.json, update package.json, server.ts, and ClerkWebhook.ts

--- api/server.ts
+++ api/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { type Request, type Response } from "express";
 import { clerkMiddleware } from "@clerk/express";
 import "dotenv/config";
 import cors from "cors";
--- api/src/controllers/ClerkWebhook.ts
+++ api/src/controllers/ClerkWebhook.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import type { Request, Response } from "express";
 import { Webhook } from "svix";
 
 import User from "../models/User.js";
